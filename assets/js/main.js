b831189c62ccbd6fd0590fbc631caa50d8cdffe9
AGD3SIGN <brandon.agdesign@gmail.com>
2024-12-02 21:46:09 -0600

    nav bar updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,18 @@ window.onload = function () {
     const preloader = document.querySelector('.preloader');
     preloader.classList.add('hidden');
 };
+
+const navBar = document.querySelector(".navbar");
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  let currScrollPos = window.scrollY;
+
+  if (currScrollPos > prevScrollPos) {
+    navBar.style.transform = `translateY(-105%)`;
+  } else {
+    navBar.style.transform = `translateY(0%)`;
+  }
+
+  prevScrollPos = currScrollPos;
+});
