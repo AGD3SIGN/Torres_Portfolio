c58a91d37c36fdec2112eb38b1f07344c9eedf13
AGD3SIGN <brandon.agdesign@gmail.com>
2024-12-22 11:28:59 -0600

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
-/* Preloader Hide/Unhide */
-window.onload = function () {
-  const preloader = document.querySelector(".preloader");
-  preloader.classList.add("hidden");
-};
+// Wait for the window to load all resources
+window.addEventListener('load', () => {
+  const preloader = document.querySelector('.preloader');
+  
+  if (preloader) {
+      // Add the 'hidden' class to apply CSS transitions
+      preloader.classList.add('hidden');
+      
+      // Remove the preloader element from the DOM after the transition ends
+      preloader.addEventListener('transitionend', () => {
+          preloader.remove();
+      });
+  }
+});
 
 
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
